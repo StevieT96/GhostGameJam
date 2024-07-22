d85976cb031454a56afd370bf0be9fe03abce7e6
Jish00Fish <124107833+Jish00Fish@users.noreply.github.com>
2024-07-22 16:36:32 +1000

    pushed AI random pathing changes

--- Assets/Scripts/AiController.cs
+++ Assets/Scripts/AiController.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Security.Cryptography;
+using Unity.Mathematics;
+
 
 public class AiController : MonoBehaviour
 {
@@ -12,9 +15,27 @@ public class AiController : MonoBehaviour
     private bool hasTargetPosition = false;
     private int scareMeter = 0;
 
+
+    [SerializeField] private GameObject AiRunPoint;
+    [SerializeField] private List<GameObject> AiWayPoints;
+
+    [SerializeField] private float distanceNeededToArriveAtWaypoint = 3;
+
+    [SerializeField] private int UpperRandomTimeToWaitBeforeMoving = 5;
+
+    private bool arrivedAtWaypoint = false;
+
+    private GameObject currentWayPoint;
+
+    float distanceToWaypoint = 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        int randomWayPoint = RandomNumberGenerator.GetInt32(0, AiWayPoints.Count);
+
+        currentWayPoint = AiWayPoints[randomWayPoint];
+
 
         hasTargetPosition = anim.GetBool("HasTargetPosition");
         scareMeter = anim.GetInteger("ScareMeter");
@@ -23,6 +44,40 @@ public class AiController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distanceToWaypoint = Vector3.Distance(transform.position, currentWayPoint.transform.position);
+
+        if (distanceToWaypoint < distanceNeededToArriveAtWaypoint)
+            ArrivedAtWaypoint();
+    }
+
+    private void MoveToNewWayPoint()
+    {
+        int randomWayPoint = RandomNumberGenerator.GetInt32(0, AiWayPoints.Count);
+
+        currentWayPoint = AiWayPoints[randomWayPoint];
+
+        NavMeshHit myNavHit;
+        if (NavMesh.SamplePosition(currentWayPoint.transform.position, out myNavHit, 100, -1))
+        {
+            agent.SetDestination(myNavHit.position);
+
+        }
+    }
+
+    private void ArrivedAtWaypoint()
+    {
+        arrivedAtWaypoint = true;
+
+        int randomWaitTime = RandomNumberGenerator.GetInt32(0, UpperRandomTimeToWaitBeforeMoving);
+
+        StartCoroutine(WaitSecondsMoveToNewWayPoint(randomWaitTime));
+
+    }
+
+    private IEnumerator WaitSecondsMoveToNewWayPoint(float _waitTime)
+    {
+        yield return new WaitForSecondsRealtime(_waitTime);
+
+        MoveToNewWayPoint();
     }
 }
